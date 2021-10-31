eeafeb1c59fe597ad2685f6df390cf5ca828c0b9
Kurt Höblinger <office@nitricware.com>
2021-10-31 10:12:46 +0100

    fixed date display bug

---
+++ wp-content/themes/wp-latex-theme/single.php
@@ -0,0 +1,36 @@
+<!--
+    This `single.php` displays posts.
+    
+    If it wouldn't exist, Wordpress would fall back to
+    `singular.php`. But since pages don't need to display
+    the same meta information as posts, this file exists.
+-->
+
+<?php get_header(); ?>
+<div class="grid grid-pad">
+    <div class="col-2-3">
+        <div class="container">
+            <article class="blog-post">
+                <h1 class="post-title"><?php the_title(); ?></h1>
+                <h2 class="post-meta"><?php the_author_meta('display_name', 1); ?></h2>
+                <h2 class="post-meta"><?php echo get_the_time('d.m.Y', get_the_ID()); ?></h2>
+		        <?php the_content(); ?>
+	            <?php edit_post_link(); ?>
+            </article>
+        </div>
+    </div>
+    <div class="col-1-3">
+        <div class="container">
+            <div class="post-teaser">
+				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar Widgets") ) : ?>
+				<?php endif;?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+	wp_footer();
+	get_footer();
+?>
+    
