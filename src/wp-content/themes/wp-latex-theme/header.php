213c38c095c6c4b589111c91247cbc7c4ed60b96
Kurt Höblinger <office@nitricware.com>
2021-11-03 15:09:15 +0100

    fixed a visible underline next to the logo

--- src/wp-content/themes/wp-latex-theme/header.php
+++ src/wp-content/themes/wp-latex-theme/header.php
@@ -22,7 +22,9 @@
 	                }
                 }
             ?>
-            <br />
+        </a>
+        <br />
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
             <?php bloginfo( 'name' ); ?>
         </a>
     </h1>
